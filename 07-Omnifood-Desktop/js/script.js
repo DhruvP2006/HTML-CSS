308f41027cc42977e55297eb0d5584512d55c132
DhruvP2006 <dhruv.pankhania@somaiya.edu>
2024-12-28 11:40:04 +0530

    Smooth Scrolling, Sticky Nav & Optimizations

--- 07-Omnifood-Desktop/js/script.js
+++ 07-Omnifood-Desktop/js/script.js
@@ -62,11 +62,20 @@ const sectionHeroEL = document.querySelector(".section-hero");
 const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
+    console.log(ent);
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
   },
   {
     // In te viewport
     root: null,
     threshold: 0,
+    rootMargin: "-80px",
   }
 );
 obs.observe(sectionHeroEL);
